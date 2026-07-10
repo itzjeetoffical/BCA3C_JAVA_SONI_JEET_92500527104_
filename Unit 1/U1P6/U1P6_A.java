4a70f02c429026cb829535c9f581e9c9e5d73462
Jeet Soni <jeetsoni273@gmail.com>
2026-07-10 20:03:09 +0530

    Add program to print name 10 times using for loop

---
+++ Unit 1/U1P6/U1P6_A.java
@@ -0,0 +1,12 @@
+/*Write a Java program to print a name 10 times using a for loop*/
+class program6Sa{
+    public static void main(String[] args) {
+
+        String name = "Keshar";
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + ". " + name);
+        }
+
+    }
+}
