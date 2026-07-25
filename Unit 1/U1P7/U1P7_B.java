f27c714bb37c6091ee7d82a830f8ffbc509b6962
Jeet Soni <jeetsoni273@gmail.com>
2026-07-25 13:29:22 +0530

    Add files via upload

---
+++ Unit 1/U1P7/U1P7_B.java
@@ -0,0 +1,17 @@
+/*write a program for if,else in java */
+import java.util.Scanner;
+
+class U1P7_B{
+public static void main(String[] args) {
+    int examscore=100;
+
+    if(examscore>=50){
+        System.out.println("Congratulations you passed");
+    }
+    else{
+        System.out.println("Sorry you are failed");
+    }
+    
+
+}
+}
