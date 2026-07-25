c15b110180e0c2e1c22e4bdc7dfb419841c1d4b3
Jeet Soni <jeetsoni273@gmail.com>
2026-07-25 13:28:26 +0530

    Create U1P7_A.java

---
+++ Unit 1/U1P7/U1P7_A.java
@@ -0,0 +1,14 @@
+/*write a program for if condition in java */
+import java.util.Scanner;
+
+class U1P7_A{
+    public static void main(String[] args) {
+
+        int age = 15;
+
+        if(age >= 15){
+            System.out.println("age is correct");
+        }
+
+    }
+} 
