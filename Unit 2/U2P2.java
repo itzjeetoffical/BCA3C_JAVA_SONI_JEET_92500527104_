ed93cdc32315c12432865f0df92a881a96164bae
Jeet Soni <jeetsoni273@gmail.com>
2026-08-15 11:55:14 +0530

    Add Animal, Dog, and Cat classes with methods

---
+++ Unit 2/U2P2.java
@@ -0,0 +1,29 @@
+class Animal{
+    void eat(){
+        System.out.println("the animal eats. ");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("the dog barks woof woof ");
+    }
+}
+class Cat extends Animal{
+    void meow(){
+        System.out.println("the cat does meow meow");
+    }
+}
+public class U2P2 {
+    public static void main(String[] args) {
+        
+    
+    Dog D = new Dog();
+    Cat C = new Cat();
+    
+    D.eat();
+    D.bark();
+    
+    C.eat();
+    C.meow();
+    }
+}
