742e91e4c016ba5ff2001786966f9ea4ded49085
Jeet Soni <jeetsoni273@gmail.com>
2026-08-15 11:46:02 +0530

    Add Java program for manually assigned 1D array
    Implemented a Java program that initializes a 1D array with manually assigned values and prints them.

---
+++ Unit 1/U1P10/U1P10_B.java
@@ -0,0 +1,18 @@
+/* WRITE A JAVA PROGRAM OF 1D ARRAY AND TAKE VALUES MANUALLY. */
+
+class U1P10_B
+{
+    public static void main(String args[])
+    {
+        int marks[] = {60, 50, 40, 30, 80};
+
+        int i;
+
+        System.out.println("MANUALLY ASSIGNED ARRAY ELEMENTS:");
+
+        for(i = 0; i < marks.length; i++)
+        {
+            System.out.println(marks[i]);
+        }
+    }
+}
